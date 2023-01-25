eb6afe632ee0e2f6e87073a42c9b45d6a45c5f45
Navneet Rajput <108276935+NavneetRajput1@users.noreply.github.com>
2023-01-25 21:02:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function threeSum(arr, target) {
   //your code here
+	let arr_size = arr.length;
+	let l, r; 
+    for (let i = 0; i < arr_size - 2; i++) 
+    { 
+        for (let j = i + 1; j < arr_size - 1; j++) 
+        { 
+            for (let k = j + 1; k < arr_size; k++) 
+            { 
+                if (arr[i] + arr[j] + arr[k] == target) 
+                { 
+                    document.write(arr[i]+arr[j]+arr[k]); 
+                    return true; 
+                } 
+            } 
+        } 
+    } 
+    return false;
 }
 
 module.exports = threeSum;
